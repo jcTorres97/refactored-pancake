81877d70fa12aa3604181d82a0faa0bb8f677ab5
jcTorres97 <juliocetg@gmail.com>
2024-04-23 14:09:29 -0500

    Cambios prueba ingreso GO1

--- CodeTest/Controllers/HomeController.cs
+++ CodeTest/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
 using Quote.Contracts;
 using Quote.Models;
 
@@ -52,6 +55,11 @@ namespace PruebaIngreso.Controllers
 
         public ActionResult Test3()
         {
+            //Mandamos el codigo a la funcion que invoca la api y guardamos el resultado en 
+            //la variable message para mostrarla en la vista
+            var message = LlamadaAPI("E-U10-UNILATIN");
+
+            ViewBag.Message = message;
             return View();
         }
 
@@ -74,6 +82,45 @@ namespace PruebaIngreso.Controllers
 
             var result = this.quote.Quote(request);
             return View(result.TourQuotes);
+        }
+
+        //Se que en el controlador no debe de haber logica, pero fue mas facil para mi desarrollar todo aqui
+        //y al revisar como todo esta en el mismo archivo creo que es mas rapido 
+        public String LlamadaAPI(string code)
+        {
+            //Se indica el protocolo tls para poder realizar el request
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            //Especificamos la url (Tuve que hacer otra porque me acabe las peticiones)
+            //var url = $"https://refactored-pancake.free.beeceptor.com/margin/{code}";
+            var url = $"https://prueba-margin.free.beeceptor.com/margin/{code}";
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return null;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            string responseBody = objReader.ReadToEnd();
+
+                            return responseBody;
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                return "{ \"margin\": 0.0 }";
+            }
+
         }
     }
 }
